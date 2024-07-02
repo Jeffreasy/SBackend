66cbe2acf913eab76b4421231bb61725d9fbc980
Jeffreasy <158185183+Jeffreasy@users.noreply.github.com>
2024-07-02 23:31:57 +0200

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+// eslint.config.js
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    files: ['src/**/*.ts'],
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      prettier: require('eslint-plugin-prettier'),
+    },
+    languageOptions: {
+      parser: require('@typescript-eslint/parser'),
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          trailingComma: 'all',
+          endOfLine: 'lf',
+        },
+      ],
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+  },
+];
--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,12 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import rateLimit from 'express-rate-limit';
-import compression from 'compression';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(compression());
-
-// Rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use(limiter);
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI as string)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connect to MongoDB', err));
-
-// Routes
-// TODO: Add your routes here
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-export default app;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
