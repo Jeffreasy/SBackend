18610f64358e545bb31cd6dfb998705e50b4a677
Jeffrey L <158185183+Jeffreasy@users.noreply.github.com>
2024-07-03 17:45:29 +0200

    Update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,18 @@
-const { Linter } = require("eslint");
-
-module.exports = [
-  {
-    ignores: ["node_modules/**"],
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
   },
-  {
-    files: ["src/**/*.ts"],
-    plugins: {
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-      prettier: require("eslint-plugin-prettier"),
-    },
-    languageOptions: {
-      parser: require("@typescript-eslint/parser"),
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-        project: "./tsconfig.json",
-      },
-    },
-    rules: {
-      "prettier/prettier": "error",
-      ...require("@typescript-eslint/eslint-plugin").configs.recommended.rules,
-      "@typescript-eslint/no-explicit-any": "warn",
-    },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
   },
-];
+};
