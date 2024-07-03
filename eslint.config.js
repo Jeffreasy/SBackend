fb6f0a87e9dc240c0e657b92b027b11958b29dfb
Jeffrey L <158185183+Jeffreasy@users.noreply.github.com>
2024-07-03 23:09:04 +0200

    Oplossing voor ESLint-fouten en workflow-aanpassingen

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,25 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
+  root: true, // Zorgt ervoor dat ESLint stopt met zoeken naar configuratiebestanden in bovenliggende mappen.
+  parser: '@typescript-eslint/parser', // Specifieke parser voor TypeScript-code.
   parserOptions: {
-    project: ['./tsconfig.json'],
+    project: ['./tsconfig.json'], // Maakt gebruik van je TypeScript configuratie.
+    tsconfigRootDir: __dirname, // Specificeert de root directory van je TypeScript-project.
+    ecmaVersion: 2021,        // Stel in op de nieuwste ECMAScript-versie die je gebruikt.
+    sourceType: 'module',     // Laat het gebruik van import/export syntax toe.
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'], // Plugins voor TypeScript-specifieke regels en Prettier.
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'eslint:recommended',           // Aanbevolen ESLint regels.
+    'plugin:@typescript-eslint/recommended', // Aanbevolen TypeScript-ESLint regels.
+    'plugin:prettier/recommended',    // Integreert Prettier in ESLint.
   ],
   rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
+    'prettier/prettier': 'error',    // Fouten rapporteren voor Prettier opmaakproblemen.
+    '@typescript-eslint/no-explicit-any': 'error', // Verbiedt het gebruik van 'any'.
+    '@typescript-eslint/no-unused-vars': 'error', // Verbiedt ongebruikte variabelen.
+    '@typescript-eslint/ban-ts-comment': 'warn',  // Waarschuwt voor het gebruik van @ts-ignore en @ts-nocheck.
+    '@typescript-eslint/no-var-requires': 'off', // Schakelt de regel uit die het gebruik van require() verbiedt (nodig voor CommonJS modules).
+    // Voeg hier eventueel andere regels toe naar je eigen voorkeuren.
   },
+  ignorePatterns: ['**/*.js'], // Negeer alle JavaScript-bestanden om conflicten te voorkomen
 };
